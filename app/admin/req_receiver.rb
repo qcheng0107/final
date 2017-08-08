fe7c89c61df662b743100af8f0b85d96d802f56b
Raghu Betina <raghu@firstdraft.com>
2017-08-08 17:22:56 +0000

    Whitelist columns in ReqReceiver for ActiveAdmin

--- app/admin/req_receiver.rb
+++ app/admin/req_receiver.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ReqReceiver do
 
+ permit_params :student_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #

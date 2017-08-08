4ac9697ce58f29784d061e2d4016279d8e8891f9
Raghu Betina <raghu@firstdraft.com>
2017-08-08 17:20:51 +0000

    Generate students resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :students
+  # Routes for the Student resource:
+  # READ
+  get "/students", :controller => "students", :action => "index"
+  get "/students/:id", :controller => "students", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

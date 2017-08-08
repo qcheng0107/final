67255e7759a809ab4491410e624c38466bc1f1ad
Raghu Betina <raghu@firstdraft.com>
2017-08-08 17:22:10 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "courses#index"
   # Routes for the Req_receiver resource:
   # CREATE
